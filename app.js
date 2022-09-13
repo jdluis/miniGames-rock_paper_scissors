ea94ec3553259ce172f40378aa348c37b45b9a2b
jdluis <jdluisdev@gmail.com>
2022-09-13 16:54:41 +0100

    new version 2.0

--- app.js
+++ app.js
@@ -1,46 +1,60 @@
-const piedra = 0;
-const papel = 1;
-const tijera = 2;
+const rock = 0;
+const paper = 1;
+const scissors = 2;
 
+const rockBtn = document.querySelector("#rock");
+const paperBtn = document.querySelector("#paper");
+const scissorsBtn = document.querySelector("#scissors");
 const btnstart = document.getElementById("start");
 const resultDOM = document.getElementById("result");
+const restartBtn = document.querySelector("#restart");
+const options = document.querySelector("#options");
 
 let player = null;
 let pc = null;
+let playerDOM = document.querySelector("#playerPoints");
+let pcDOM = document.querySelector("#pcPoints");
+let playerPoints = 0;
+let pcPoints = 0;
 
-function winStringToDOM(winner, looser, name) {
+function drawWinner(winner, looser, name) {
   resultDOM.innerText = "";
 
+  //put the name to the variable
   switch (winner) {
-    case piedra:
+    case rock:
       winner = "rock";
       break;
-    case papel:
+    case paper:
       winner = "paper";
       break;
-    case tijera:
+    case scissors:
       winner = "scissors";
       break;
   }
 
   switch (looser) {
-    case piedra:
+    case rock:
       looser = "rock";
       break;
-    case papel:
+    case paper:
       looser = "paper";
       break;
-    case tijera:
+    case scissors:
       looser = "scissors";
       break;
   }
 
   if (name != "empate" && name == "Player") {
+    playerDOM.innerText = ++playerPoints;
+
     return (resultDOM.innerText = `
     Player select: ${winner}
     PC select: ${looser}
     The winner is: ${name} `);
   } else if (name != "empate" && name == "PC") {
+    pcDOM.innerText = ++pcPoints;
+
     return (resultDOM.innerText = `
     Player select: ${looser}
     PC select: ${winner}
@@ -55,13 +69,13 @@ function winStringToDOM(winner, looser, name) {
 
 function winCondition() {
   if (player == pc) {
-    winStringToDOM(pc, player, "empate");
+    drawWinner(pc, player, "empate");
   } else if (player - pc == 1) {
-    winStringToDOM(player, pc, "Player");
+    drawWinner(player, pc, "Player");
   } else if (player - pc == -2) {
-    winStringToDOM(player, pc, "Player");
+    drawWinner(player, pc, "Player");
   } else {
-    winStringToDOM(pc, player, "PC");
+    drawWinner(pc, player, "PC");
   }
 }
 
@@ -70,11 +84,41 @@ function getRandomInt(min, max) {
   return number;
 }
 
-//START
+function getAttributeOnEvent() {
+  rockBtn.addEventListener("click", () => {
+    player = parseInt(rockBtn.getAttribute("--data-value"));
+    pc = getRandomInt(0, 3);
+    winCondition();
+  });
+
+  paperBtn.addEventListener("click", () => {
+    player = parseInt(paperBtn.getAttribute("--data-value"));
+    pc = getRandomInt(0, 3);
+    winCondition();
+  });
+
+  scissorsBtn.addEventListener("click", () => {
+    player = parseInt(scissorsBtn.getAttribute("--data-value"));
+    pc = getRandomInt(0, 3);
+    winCondition();
+  });
+}
+
+function reloadEvent (btn) {
+  btn.addEventListener("click", () => {
+    window.location.reload();
+  })
+}
+
+//Init
 btnstart.addEventListener("click", () => {
-  player = getRandomInt(0,3);
-  pc = getRandomInt(0,3);
-  console.log("pc" + ": " + pc)
-  console.log("player" + ": " + player)
-  winCondition(player, pc);
+  btnstart.style.display = "none";
+  options.style.display = "flex";
+  restartBtn.style.display = "block";
+
+  playerDOM.innerText = playerPoints;
+  pcDOM.innerText = pcPoints;
+
+  getAttributeOnEvent();
+  reloadEvent(restartBtn);
 });
